752c66c93c8d781881bab24d048344b17bc7916e
sendosend <johnsenberdin2930@gmail.com>
2025-11-03 23:54:58 +0800

    tweaking the form

--- typscript/ts-docs/src/Components/Docs.tsx
+++ typscript/ts-docs/src/Components/Docs.tsx
@@ -1,17 +1,33 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { docsContext } from "./context/docsContext";
 import Categories from "./Categories";
+import Form from "./Form/Form";
 
 const Docs = () => {
   const context = useContext(docsContext);
+  const [showForm, setShowForm] = useState(false);
+  const handleForm = () => setShowForm((c) => !c);
 
   return (
-    <section className="p-5">
-      <button className="mx-auto block" onClick={context.handleToggle}>
-        Get started
-      </button>
-      {context.toggle && <Categories />}
-    </section>
+    <>
+      <section className="px-5">
+        <button className="mx-auto block" onClick={context.handleToggle}>
+          Get started
+        </button>
+        {context.toggle && <Categories />}
+
+        <div className="flex justify-center mt-5">
+          <button className="" onClick={handleForm}>
+            {showForm ? "Hide Form" : "Add Doc"}
+          </button>
+        </div>
+        {showForm && (
+          <div className="w-full mt-5">
+            <Form />
+          </div>
+        )}
+      </section>
+    </>
   );
 };
 
